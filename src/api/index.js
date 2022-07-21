09efd91546fc59aeafaa121321aaa3682ca1a17e
ShootingersNew <lebedevseergey@gmail.com>
2022-07-21 08:15:37 +0300

    fix imports

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,17 @@
-const filters = await import('./filters.json')
-const users = await import('./users.json')
+
 class Api {
+    //В идеале код в настоящем приложении должен быть разбитым на модули, но это тз
     filters = {
         async getFilters() {
+            //Тут должен был дергаться эндпоинт, но раз попросили имитировать асинхронную загрузку
+            const filters = await import('./filters.json')
             return filters.allFilters
         },
     }
     users = {
         async getUsers(filters) {
+            //Тут должен был дергаться эндпоинт, но раз попросили имитировать асинхронную загрузку
+            const users = await import('./users.json')
             const getIsSuitable = (user) => {
                 return filters.every(({ name, value }) => {
                     return value? user[name] === value : true
